cc646b573e57671241c61d2b56f461119039f01f
user az30314 <user@az30314.com>
2024-12-20 22:17:28 -0800

    Added support for Raspberry Pi Pico 2 RS2350 microcontroller

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -105,7 +105,13 @@
 #define configNUM_CORES                         2
 #define configTICK_CORE                         0
 #define configRUN_MULTIPLE_PRIORITIES           1
-#define configUSE_CORE_AFFINITY                 1
+#define configUSE_CORE_AFFINITY                 0
+#define configENABLE_MPU                        0
+#define configENABLE_TRUSTZONE                  0
+#define configRUN_FREERTOS_SECURE_ONLY          1
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    16
+#define configENABLE_FPU                        1
+
 
 /* RP2040 specific */
 #define configSUPPORT_PICO_SYNC_INTEROP         1
